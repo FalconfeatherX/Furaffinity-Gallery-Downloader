463b83c0e4c1d727b47bc02ac41662b537135725
Falconfeather <41564661+FalconfeatherX@users.noreply.github.com>
2019-03-13 14:52:52 +0800

    Update Constant.py

--- Constant.py
+++ Constant.py
@@ -14,7 +14,7 @@ PASSWORD = ''
 DB       = ''
 
 TEXT     = '''Furaffinity Gallery Downloader use multiprocess tech
-        to scrape full size download urls which will be inset into mysql database
+        to scrape full size download urls which will be inserted into mysql database
         and download artwork from the database rapidly.'''
 
 SCRAPS   = False
