0106673cf3b9db0ffa6b5f0a3311dabd7ee27fef
Falconfeather <41564661+FalconfeatherX@users.noreply.github.com>
2020-05-11 21:54:16 +0800

    Update Download.py
    fix fragmentary downloaded file problem caused by network fluctuation.

--- Download.py
+++ Download.py
@@ -1,10 +1,9 @@
-# -*- coding: utf-8 -*-
 from Kits import Kits
 import time
 from multiprocessing import Pool,Process,Manager
+import os
 import requests
 from itertools import repeat
-import Constant
 
 class Download:
     version = 1.0
@@ -15,38 +14,56 @@ class Download:
 
     @staticmethod
     def connect(url):
-        return requests.Session().get(url,timeout = 30)
+        return requests.session().get(url,timeout = 15)
 
     def single_download(self,args):
+        time.sleep(0.5)
         results,list_share = args
         print(results)
         AL = self.artist_lenth
-        Startline = AL + 43
-
-        if not results[Startline:]:
-            print('Name Error')
-            return False
+        Startline = AL + 43#43
+        Startline_for_long = AL + 60
+        size = 0 #确保size永远有定义
 
         try:
             source = Download.connect(results)
-        except:
-            print('Connected error' + results)
+            file_type = source.headers["content-type"]
+        except requests.exceptions.RequestException:
+            print("timeout")
+            return
 
-        try:
-            with open(self.path + '%s'%(results[Startline:]), 'wb') as dest:
-                dest.write(source.content)
-                if source:
-                    list_share.append(results)
-                    time.sleep(Constant.INTERVAL)
-        except IOError as e:
-            print('IOError:',e)
+        if not source.status_code == 200:
+            print("connect error %s"%(results))
+            return
+
+        if not '/' in results[Startline:]:
+            file_path = self.path + '%s'%(results[Startline:])
+            size   = source.headers["Content-Length"]           #目标文件大小
+
+        else:
+            file_path = self.path + '%s'%(results[Startline_for_long:])
+            list_share.append(results)
+
+        with open(file_path, 'wb') as dest:
+            dest.write(source.content)
+        if file_type == "text/plain":
+            return
+
+        file_size = os.path.getsize(file_path)              #实际文件大小
+
+        if file_size == int(size) and file_size != 0:
+            list_share.append(results)
+        else:                                               #大小不匹配，则重新下载
+            print("size difference! goal:%d downloaded:%d url:%s"%(int(size),file_size,results))
+
 
     def multi_download(self,list_result):
         list_share = Manager().list()
         list_total = len(list_result)
         data = zip(list_result,repeat(list_share))
+        print(list(list_result))
         #vice_Processing = Process(target = Processingbar,args = ((list_share,len(list_total),))
-        workers = Pool(Constant.THREADS)
+        workers = Pool(8)
         #vice_Processing.start()
         workers.map(self.single_download,data)
         workers.close()
