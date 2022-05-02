34528f671564176bb62f4ca631d3a3f08274fe0d
xieyi <1098787037@qq.com>
2022-05-02 19:27:19 +0800

    fix bugs

--- Constant.py
+++ Constant.py
@@ -1,25 +1,18 @@
 
-INTERVAL = 1
-
+INTERVAL = 0.5 #爬取的默认间隔，下载默认为0.5，大于默认可能被服务器拦截
 URL      = 'http://www.furaffinity.net/'
-
-THREADS  = 4
-
-MAXPAGE  = 40
-
+THREADS  = 6 #爬取的默认最大进程数，下载默认为8，大于默认可能被服务器拦截
+MAXPAGE  = 40 #默认最大为40页，请视情况更改
 PATH     = ''
-
 PASSWORD = ''
-
 DB       = ''
-
+HOST     = ''
 TEXT     = '''Furaffinity Gallery Downloader use multiprocess tech
-        to scrape full size download urls which will be inserted into mysql database
-        and download artwork from the database rapidly.'''
-
+        to scrape urls of artwork which will be inserted into mysql database
+        and download artwork by urls fetched from the database rapidly.'''
 SCRAPS   = False
-
 FAVS     = False
+WINPATH  =''
 
 LOG_CONGIF = {
     'version': 1,
